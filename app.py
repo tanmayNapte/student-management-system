491b096b619ab49d656a121091ded65ba55458ba
tanmayNapte <tanmaynapte232@gmail.com>
2025-12-17 13:16:04 +0530

    Update edit/add form and full name logic

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 # connect to database
 def get_db():
-    return sqlite3.connect("students.db")
+    return sqlite3.connect("students1.db")
 
 # create table (runs once)
 with get_db() as con:
@@ -28,19 +28,56 @@ def index():
 
 @app.route("/add", methods=["GET", "POST"])
 def add_student():
+
+    con = get_db()
     if request.method == "POST":
-        name = request.form["name"]
+        first = request.form["first_name"]
+        middle = request.form["middle_name"]
+        last = request.form["last_name"]
+
+        full_name = " ".join(part for part in [first, middle, last] if part)
         age = request.form["age"]
         course = request.form["course"]
-
-        con = get_db()
         con.execute(
-            "INSERT INTO students (name, age, course) VALUES (?, ?, ?)",(name, age, course)
+            "INSERT INTO students (name, age, course) VALUES (?, ?, ?)",
+            (full_name, age, course)
         )
+
         con.commit()
         return redirect("/")
 
     return render_template("add.html")
+
+@app.route("/edit/<int:id>", methods=["GET", "POST"])
+def edit_student(id):
+    con = get_db()
+
+    if request.method == "POST":
+        first = request.form["first_name"]
+        middle = request.form["middle_name"]
+        last = request.form["last_name"]
+
+        full_name = " ".join(part for part in [first, middle, last] if part)
+
+        age = request.form["age"]
+        course = request.form["course"]
+
+        con.execute(
+            "UPDATE students SET name = ?, age = ?, course = ? WHERE id = ?",
+            (full_name, age, course, id)
+        )
+        con.commit()
+        con.close()
+        return redirect("/")
+
+    student = con.execute(
+        "SELECT * FROM students WHERE id = ?",
+        (id,)
+    ).fetchone()
+    con.close()
+
+    return render_template("edit.html", student=student)
+
 
 @app.route("/delete/<int:id>")
 def delete_student(id):
